a027ec184d4854681a3cd0d64599b5a0b311c92a
AllanGamal <63316423+AllanGamal@users.noreply.github.com>
2020-08-10 22:27:02 +0200

    ...

---
+++ whygoogle/script.js
@@ -0,0 +1,71 @@
+let questions = [
+  { question: "What is my ip?", searches: 4550, img: "ip", a: "ip" },
+  { question: "What time is it?", searches: 878, img: "time", a: "time" },
+  { question: "What is VPN?", searches: 164, img: "vpn", a: "vpn" },
+  { question: "What day is it today?", searches: 161, img: "day", a: "day" },
+
+  { question: "Why is the skye blue?", searches: 694, img: "sky", a: "sky" },
+  { question: "Why do cats purr?", searches: 526, img: "cat", a: "cat" },
+  { question: "Why do we sleep?", searches: 358, img: "sleep", a: "sleep" },
+  { question: "Why do dogs eat grass?", searches: 348, img: "dog", a: "dog" },
+  { question: "Why is my poop green?", searches: 347, img: "poop", a: "poop" },
+  { question: "Why am I so tired?", searches: 314, img: "tired", a: "tired" },
+
+  {
+    question: "How many days in a year?",
+    searches: 113,
+    img: "days",
+    a: "days",
+  },
+  { question: "How to tie a tie?", searches: 67, img: "tie", a: "tie" },
+  {
+    question: "How many oucnes in a pound?",
+    searches: 63,
+    img: "ounces",
+    a: "pound",
+  },
+  {
+    question: "How How to screenshot on mac?",
+    searches: 57,
+    img: "screenshot",
+    a: "screenshot",
+  },
+  {
+    question: "How How many ounces in a cup?",
+    searches: 55,
+    img: "ounces",
+    a: "cup",
+  },
+  {
+    question: "How How many ounces in a gallon?",
+    searches: 51,
+    img: "ounces",
+    a: "gallon",
+  },
+
+  {
+    question: "When is black friday?",
+    searches: 749,
+    img: "blackfriday",
+    a: "blackfriday",
+  },
+  {
+    question: "When is thanksgiving?",
+    searches: 570,
+    img: "thanksgiving",
+    a: "thanksgiving",
+  },
+  { question: "When is easter?", searches: 341, img: "easter", a: "easter" },
+  {
+    question: "When is halloween?",
+    searches: 240,
+    img: "halloween",
+    a: "halloween",
+  },
+  {
+    question: "When do the clocks go back?",
+    searches: 216,
+    img: "clock",
+    a: "clock",
+  },
+];
